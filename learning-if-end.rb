4e5c88021e55f894dcb766969274b7edcc73b92b
Andrea Toston <tostonar@gmail.com>
2020-10-20 18:51:43 +0000

    Automatically backed up by Learn

---
+++ learning-if-end.rb
@@ -0,0 +1,9 @@
+# run_code_inside = false
+# puts "Code before if...end"
+# if run_code_inside
+#   puts "code inside"
+# end
+# puts "Code after if...end"
+puts "You know what year it is??"
+this_year = Time.now.year
+puts "Hey, it's 2019!" if this_year == 2019
---
+++ while.rb
@@ -0,0 +1,7 @@
+magic_exit_number = 7
+count = 0
+while count < 10 && count != magic_exit_number do
+ 
+  puts "I am the #{count}, I love to count!" # Work
+  count +=  1
+end
